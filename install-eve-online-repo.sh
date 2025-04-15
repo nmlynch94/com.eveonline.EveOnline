95d7cc2da42e3014337c0ce9ad3f282f38ff641b
nmlynch94 <40608755+nmlynch94@users.noreply.github.com>
2025-04-15 14:40:25 -0400

    Support nvidia open driver

--- install-eve-online-repo.sh
+++ install-eve-online-repo.sh
@@ -6,7 +6,7 @@ set -e
 HAS_NVIDIA=0
 if [[ -f /proc/driver/nvidia/version ]]; then
     HAS_NVIDIA=1
-    NVIDIA_VERISON=$(cat /proc/driver/nvidia/version | head -n 1 | awk '{ print $8 }' | sed 's/\./-/g')
+    NVIDIA_VERSION=$(sed -n 's/^NVRM version:.* \([0-9]\+\.[0-9]\+\.[0-9]\+\) .*/\1/p' /proc/driver/nvidia/version | sed 's/\./-/g')
 fi
 
 flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
@@ -20,8 +20,8 @@ flatpak install --user -y --noninteractive flathub \
 
 if [[ ${HAS_NVIDIA} -eq 1 ]]; then
     flatpak install --user -y --noninteractive flathub \
-        org.freedesktop.Platform.GL.nvidia-${NVIDIA_VERISON}/x86_64 \
-        org.freedesktop.Platform.GL32.nvidia-${NVIDIA_VERISON}/x86_64
+        org.freedesktop.Platform.GL.nvidia-${NVIDIA_VERSION}/x86_64 \
+        org.freedesktop.Platform.GL32.nvidia-${NVIDIA_VERSION}/x86_64
 fi
 
 flatpak install -y --user --noninteractive EveOnline com.eveonline.EveOnline
