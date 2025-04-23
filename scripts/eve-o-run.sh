6dca1ee1a15cb0b9b4706f634e406b405f320bc1
nmlynch94 <nmlynch94@gmail.com>
2025-04-22 21:03:48 -0400

    Fix proton path TODO dynamically find path

--- scripts/eve-o-run.sh
+++ scripts/eve-o-run.sh
@@ -9,5 +9,4 @@ cp /app/eve-o-preview.exe "$XDG_DATA_HOME"/prefix/eve-o-preview.exe
 
 # For some reason this file needs to be created or dotnet fails to run
 mkdir -p "$XDG_DATA_HOME/home/$USER/.cache/dotnet_bundle_extract"
-
-"$XDG_DATA_HOME"/proton/files/bin/wine64 "$XDG_DATA_HOME"/prefix/eve-o-preview.exe
+"$XDG_DATA_HOME/../.local/share/Steam/compatibilitytools.d/GE-Proton9-27/files/bin/wine64" "$XDG_DATA_HOME"/prefix/eve-o-preview.exe
