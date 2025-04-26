471f1154cab26ca68766897e369c66281c08d70a
nmlynch94 <nmlynch94@gmail.com>
2025-04-26 03:03:09 -0400

    Renovate custom matcher config

--- scripts/versions.py
+++ scripts/versions.py
@@ -1,14 +1,33 @@
 #!/usr/bin/env python3
 
 import os
+import urllib.request
+import pprint
+
+
+def sha256_file(url):
+    import hashlib
+
+    sha256 = hashlib.sha256()
+    with urllib.request.urlopen(url) as response:
+        while chunk := response.read(8192):
+            sha256.update(chunk)
+    return sha256.hexdigest()
+
 
 versions = {
-    # renovate: datasource=github-tags depName=proopai/eve-o-preview
-    "EVE_O_PREVIEW_VERSION": "8.0.2.1",
-    "EVE_O_PREVIEW_SHA256": "118091b9668c39e4b23a1c7d26b6fb024a71753d6bf0f0ff44f30f746065e4cb",
-    # renovate: datasource=github-tags depName=Open-Wine-Components/umu-launcher
-    "UMU_LAUNCHER_VERSION": "1.2.6",
-    "UMU_LAUNCHER_SHA256": "ae0bfd9bd3de209d0b6590ffbffc395d79c501b10176e9e239e4a1f842b4ad3a",
+    "eve-o-preview": {
+        "version_placeholder": "EVE_O_PREVIEW_VERSION",
+        # renovate: datasource=github-releases depName=eve-o-preview packageName=proopai/eve-o-preview
+        "version": "8.0.2.0",
+        "url_template": "https://github.com/Proopai/eve-o-preview/releases/download/{version}/Release-{version}-Linux.zip",
+    },
+    "umu-launcher": {
+        "version_placeholder": "UMU_LAUNCHER_VERSION",
+        # renovate: datasource=github-tags depName=Open-Wine-Components/umu-launcher
+        "version": "1.2.5",
+        "url_template": "https://github.com/Open-Wine-Components/umu-launcher/releases/download/{version}/umu-launcher-{version}-zipapp.tar",
+    },
 }
 
 cwd = os.getcwd()
@@ -16,6 +35,8 @@ cwd = os.getcwd()
 with open(f"{cwd}/templates/com.eveonline.EveOnline.yml", "r") as f:
     data = f.read()
     for key, value in versions.items():
-        data = data.replace(f"${{{key}}}", value)
-    with open(f"{cwd}/templates/com.eveonline.EveOnline.yml", "w") as f:
+        data = data.replace(f"${{{value['version_placeholder']}}}", value["version"])
+        sha256 = sha256_file(value["url_template"].format(version=value["version"]))
+        data = data.replace(f"${{{value['version_placeholder'] + '_SHA256'}}}", sha256)
+    with open(f"{cwd}/com.eveonline.EveOnline.yml", "w") as f:
         f.write(data)
