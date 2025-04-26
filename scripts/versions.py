f53ebcdb1695a51cbd13c120bcf5df2b03e0ff52
nmlynch94 <nmlynch94@gmail.com>
2025-04-26 04:16:35 -0400

    update

--- scripts/versions.py
+++ scripts/versions.py
@@ -17,33 +17,26 @@ def sha256_file(url):
 
 versions = {
     "eve-o-preview": {
+        "version_placeholder": "EVE_O_PREVIEW_VERSION",
         # renovate: datasource=github-releases depName=eve-o-preview packageName=proopai/eve-o-preview
         "version": "8.0.2.0",
         "url_template": "https://github.com/Proopai/eve-o-preview/releases/download/{version}/Release-{version}-Linux.zip",
     },
     "umu-launcher": {
+        "version_placeholder": "UMU_LAUNCHER_VERSION",
         # renovate: datasource=github-tags depName=Open-Wine-Components/umu-launcher
         "version": "1.2.6",
         "url_template": "https://github.com/Open-Wine-Components/umu-launcher/releases/download/{version}/umu-launcher-{version}-zipapp.tar",
     },
 }
 
-print("Versions:")
-for key, value in versions.items():
-    pprint.pprint(value)
-    print(f"{key}: {value['version']}")
-    url = value["url_template"].format(version=value["version"])
-    sha256 = sha256_file(url)
-    print(f"  {url}")
-    print(f"  {sha256}")
-    versions[key]["sha256"] = sha256
-    versions[key]["url"] = url
-
 cwd = os.getcwd()
-#
-# with open(f"{cwd}/templates/com.eveonline.EveOnline.yml", "r") as f:
-#     data = f.read()
-#     for key, value in versions.items():
-#         data = data.replace(f"${{{key}}}", value)
-#     with open(f"{cwd}/templates/com.eveonline.EveOnline.yml", "w") as f:
-#         f.write(data)
+
+with open(f"{cwd}/templates/com.eveonline.EveOnline.yml", "r") as f:
+    data = f.read()
+    for key, value in versions.items():
+        data = data.replace(f"${{{value['placeholder']}}}", value["version"])
+        sha256 = sha256_file(value["url_template"].format(version=value["version"]))
+        data = data.replace(f"${{{value['placeholder'] + '_SHA256'}}}", sha256)
+    with open(f"{cwd}/com.eveonline.EveOnline.yml", "w") as f:
+        f.write(data)
