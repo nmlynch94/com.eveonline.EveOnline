8c13ec82dcd5b52999ce83316887d32d0d740078
nmlynch94 <nmlynch94@gmail.com>
2025-04-26 03:29:15 -0400

    update

--- scripts/versions.py
+++ scripts/versions.py
@@ -2,14 +2,17 @@
 
 import os
 
-versions = {
-    # renovate: datasource=github-releases depName=eve-o-preview packageName=proopai/eve-o-preview
-    "EVE_O_PREVIEW_VERSION": "8.0.2.1",
-    "EVE_O_PREVIEW_SHA256": "118091b9668c39e4b23a1c7d26b6fb024a71753d6bf0f0ff44f30f746065e4cb",
-    # renovate: datasource=github-tags depName=Open-Wine-Components/umu-launcher
-    "UMU_LAUNCHER_VERSION": "1.2.6",
-    "UMU_LAUNCHER_SHA256": "ae0bfd9bd3de209d0b6590ffbffc395d79c501b10176e9e239e4a1f842b4ad3a",
-}
+# renovate: datasource=github-releases depName=eve-o-preview packageName=proopai/eve-o-preview
+EVE_O_PREVIEW_VERSION = ("8.0.2.1",)
+EVE_O_PREVIEW_SHA256 = (
+    "118091b9668c39e4b23a1c7d26b6fb024a71753d6bf0f0ff44f30f746065e4cb",
+)
+# renovate: datasource=github-tags depName=Open-Wine-Components/umu-launcher
+UMU_LAUNCHER_VERSION = "1.2.6"
+UMU_LAUNCHER_SHA256 = (
+    "ae0bfd9bd3de209d0b6590ffbffc395d79c501b10176e9e239e4a1f842b4ad3a",
+)
+versions = {}
 
 cwd = os.getcwd()
 
