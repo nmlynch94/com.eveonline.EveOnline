4497acb6537ed9784822dae5dbff541c5c7a6c5d
nmlynch94 <nmlynch94@gmail.com>
2025-10-02 23:06:28 -0400

    chore: return to umu. Use the same prefix to avoid reinstalling.

--- scripts/constants.sh
+++ scripts/constants.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export GAMEID="8500" # Steam ID of EVE Online
-export WINEPREFIX="$XDG_DATA_HOME"/prefix
+export WINEPREFIX="$XDG_DATA_HOME"/staging_prefix
  # At time of writing, fsync and esync cause cpu increases that never decrease
 export PROTON_NO_FSYNC=1
 export PROTON_NO_ESYNC=1
--- scripts/eve-o-run.sh
+++ scripts/eve-o-run.sh
@@ -2,14 +2,14 @@
 
 set -eua pipefail
 
-# This is not in constants because it needs to be run after umu runs for the first time
-PROTON_VERSION="$(cat "$XDG_DATA_HOME"/prefix/version)"
-
 cd "$XDG_DATA_HOME"
 
 . /app/constants.sh
-cp /app/eve-o-preview.exe "$XDG_DATA_HOME"/prefix/eve-o-preview.exe
+cp /app/eve-o-preview.exe "$WINEPREFIX/eve-o-preview.exe"
+
+# This is not in constants because it needs to be run after umu runs for the first time
+PROTON_VERSION="$(cat $WINEPREFIX/version)"
 
 # For some reason this file needs to be created or dotnet fails to run
 mkdir -p "$XDG_DATA_HOME/home/$USER/.cache/dotnet_bundle_extract"
-"$XDG_DATA_HOME/../.local/share/Steam/compatibilitytools.d/$PROTON_VERSION/files/bin/wine64" "$XDG_DATA_HOME"/prefix/eve-o-preview.exe
+"$XDG_DATA_HOME/../.local/share/Steam/compatibilitytools.d/$PROTON_VERSION/files/bin/wine64" "$WINEPREFIX/eve-o-preview.exe"
